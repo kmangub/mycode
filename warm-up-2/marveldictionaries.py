574d213d227985bd0518117effcc4765bd4a3b85
Karlo Mangubat <karlomangubat92@gmail.com>
2021-08-03 15:29:12 +0000

    finished warm-up

---
+++ warm-up-2/marveldictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+marvelchars= {
+"Starlord":
+  {"real name": "peter quill",
+  "powers": "dance moves",
+  "archenemy": "Thanos"},
+
+"Mystique":
+  {"real name": "raven darkholme",
+  "powers": "shape shifter",
+  "archenemy": "Professor X"},
+
+"She-Hulk":
+  {"real name": "jennifer walters",
+  "powers": "super strength & intelligence",
+  "archenemy": "Titania"}
+             }
+play_again = True
+
+while play_again:
+    char_name = input("Which character do you want to know about? (Starlord, Mystique, or She-Hulk \n").title()
+    char_stat = input("What statistic do you want to know about? (real name, powers, archenemy \n").lower()
+
+    print(f"{char_name.title()}'s {char_stat} is: {marvelchars[char_name][char_stat]}\n")
+
+    ask = input("Would you like to play again? (y or n) \n ")
+
+    if ask == "n":
+        play_again = False
+
+
