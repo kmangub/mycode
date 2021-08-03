097c5921c93468a9f98e5222d9043395b556e619
Karlo Mangubat <karlomangubat92@gmail.com>
2021-08-03 17:04:51 +0000

    copying local files with python

--- warm-up-2/marveldictionaries.py
+++ warm-up-2/marveldictionaries.py
@@ -19,10 +19,10 @@ marvelchars= {
 play_again = True
 
 while play_again:
-    char_name = input("Which character do you want to know about? (Starlord, Mystique, or She-Hulk \n").title()
+    char_name = input("Which character do you want to know about? (Starlord, Mystique, or She-Hulk \n").lower()
     char_stat = input("What statistic do you want to know about? (real name, powers, archenemy \n").lower()
 
-    print(f"{char_name.title()}'s {char_stat} is: {marvelchars[char_name][char_stat]}\n")
+    print(f"{char_name.title()}'s {char_stat.title()} is: {marvelchars[char_name.title()][char_stat]}\n")
 
     ask = input("Would you like to play again? (y or n) \n ")
 
